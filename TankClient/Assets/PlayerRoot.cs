efb53acbb733c357fbaad6078ddbce04495d3b07
unknown <liaochongcan@ztgame.com>
2020-08-19 20:26:45 +0800

    [room] multiplayer support

---
+++ TankClient/Assets/PlayerRoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRoot : MonoBehaviour {
+	public GameObject tank;
+	public static int myself;
+	public static Dictionary<int,GameObject> dic = new Dictionary<int, GameObject>();
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
