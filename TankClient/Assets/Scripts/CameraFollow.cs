bc80a9f946a06744c17d16e561b83d5688c743d0
unknown <liaochongcan@ztgame.com>
2020-08-19 14:51:32 +0800

    [move] ver1 update

---
+++ TankClient/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = player.position;
+		pos.y += 4;
+		pos.z -= 6;
+		GetComponent<Transform>().position = pos;
+
+    }
+}
